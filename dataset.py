55a946aa42b1262c6d07a0f0c7b6b79da382a0db
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-02-07 14:33:07 +0100

    fix warning on dataset module

--- dataset.py
+++ dataset.py
@@ -8,11 +8,9 @@ Created on Wed Oct 27 15:58:42 2021
 import os
 from nltk.corpus import stopwords
 from rdflib.plugins.parsers.ntriples import NTriplesParser, Sink
-
+from stop_words import get_stop_words
 from helpers import Utils
 from config import config
-from stop_words import get_stop_words
-
 
 utils = Utils()
 
@@ -67,20 +65,20 @@ class ESBenchmark(object):
         class IndexSink(Sink):
             i = 0
             j = 0
-            def triple(self,s,p,o):
-                sub = s.toPython()
-                pred = p.toPython()
-                obj = o.toPython()
+            def triple(self,sub,pred,obj):
+                sub = sub.toPython()
+                pred = pred.toPython()
+                obj = obj.toPython()
                 triple_tuple = (sub, pred, obj)
                 triples.append(triple_tuple)
         triples = []
         IndexSink = IndexSink()
         parser = NTriplesParser(IndexSink)
-        with open(os.path.join(db_path, "{}".format(num), "{}_desc.nt".format(num)), 'rb') as reader:
+        with open(os.path.join(db_path,"{}".format(num),"{}_desc.nt".format(num)),'rb') as reader:
             parser.parse(reader)
         return triples
     def get_labels(self, ds_name, num):
-        triples = self._get_triples(ds_name, num)
+        triples = self.get_triples(ds_name, num)
         utils = Utils()
         endpoint = "http://dbpedia.org/sparql"
         triples_tuple = []
@@ -109,7 +107,7 @@ class ESBenchmark(object):
         train_eids, valid_eids, _ = self.get_5fold_train_valid_test_elist(ds_name)
         train_data = []
         valid_data = []
-        for fold, eids_per_fold in enumerate(train_eids):
+        for eids_per_fold in train_eids:
             train_data_perfold = []
             edesc = {}
             for eid in eids_per_fold:
@@ -128,9 +126,9 @@ class ESBenchmark(object):
         return train_data, valid_data
     def get_testing_dataset(self, ds_name):
         _, _, test_eids = self.get_5fold_train_valid_test_elist(ds_name)
-        test_data = list()
-        for fold, eids_per_fold in enumerate(test_eids):
-            test_data_perfold = list()
+        test_data = []
+        for eids_per_fold in test_eids:
+            test_data_perfold = []
             for eid in eids_per_fold:
                 triples = self.get_triples(ds_name, eid)
                 edesc = {}
@@ -164,8 +162,8 @@ class ESBenchmark(object):
             parser = NTriplesParser(IndexSink)
             with open(os.path.join(db_path, "{}".format(num), "{}_gold_top{}_{}.nt".format(num, top_n, i)), 'rb') as reader:
                 parser.parse(reader)
-            for s, p, o in triples:
-                utils.counter(per_entity_label_dict, "{}++$++{}".format(p, o))
+            for _, pred, obj in triples:
+                utils.counter(per_entity_label_dict, "{}++$++{}".format(pred, obj))
         return per_entity_label_dict
     def get_predicates_corpus(self, ds_name):
         train_data, _ = self.get_training_dataset(ds_name)
@@ -195,11 +193,11 @@ class ESBenchmark(object):
                     d_sub_eid = []
                     d_obj_eid = []
                     for t in t_fold[eid]:
-                        s, _, o = t
-                        s = utils.extract_triple_to_word(s.lower())
-                        o = utils.extract_triple_to_word(o.lower())
-                        d_sub_eid.append(s)
-                        d_obj_eid.append(o)
+                        sub, _, obj = t
+                        sub = utils.extract_triple_to_word(sub.lower())
+                        obj = utils.extract_triple_to_word(obj.lower())
+                        d_sub_eid.append(sub)
+                        d_obj_eid.append(obj)
                     sub_words = [w for w in d_sub_eid if not w in self.get_stop_words()]
                     obj_words = [w for w in d_obj_eid if not w in self.get_stop_words()]
                     d_sub_fold.append(sub_words)
