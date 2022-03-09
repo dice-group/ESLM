8162b63023c234f32cacc7031aa26438b9107f7f
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-03-09 07:03:16 +0100

    updated file to execute Bert with GAT including subject on triple encoding

--- run_bert_gates_including_subject.py
+++ run_bert_gates_including_subject.py
@@ -115,7 +115,7 @@ class BertGATES(nn.Module):
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.bert_drop = nn.Dropout(0.4)
         self.out = nn.Linear(768, 1)
-        self.input_size = 29952
+        self.input_size = 768 #29952
         self.hidden_layer = config["hidden_layer"]
         self.nheads = config["nheads"]
         self.dropout = config["dropout"]
@@ -124,9 +124,10 @@ class BertGATES(nn.Module):
     def forward(self, adj, input_ids, segment_ids=None, input_mask=None):
         """forward"""
         outputs = self.bert(input_ids, segment_ids, input_mask)
-        bert_out = self.bert_drop(outputs.last_hidden_state)
+        bert_out = self.bert_drop(outputs.pooler_output)
+        bert_out = self.out(bert_out)
         #bert_out = torch.transpose(bert_out, 0, 1)
-        bert_out = torch.flatten(bert_out, start_dim=1)
+        #bert_out = torch.flatten(bert_out, start_dim=1)
         #bert_out = self.out(bert_out)
         edge = adj.data
         adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
