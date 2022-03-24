7d80d95184713be55417b8eb036c40473de2691e
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-03-24 13:50:06 +0100

    implements finetuned bert combining with GAT

--- run_bert_gates_including_subject.py
+++ run_bert_gates_including_subject.py
@@ -146,7 +146,7 @@ class BertGATES(nn.Module):
             with torch.no_grad():
                 model.eval()
                 cls_feats = model.bert_model(input_ids, input_mask)[0][:, 0]
-        features = self.classifier(cls_feats)
+        #features = self.classifier(cls_feats)
         #cls_pred = nn.Softmax(dim=0)(cls_logit)
         edge = adj.data
         adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
