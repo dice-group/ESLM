9b89e065f373520009be9eda88ef8dad28949106
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-03-31 18:22:51 +0200

    implement cosine distance

--- run_bert_including_subject.py
+++ run_bert_including_subject.py
@@ -12,6 +12,7 @@ import datetime
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as f
 from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup
 from transformers import BertModel
 from transformers import AutoModel, AutoTokenizer
