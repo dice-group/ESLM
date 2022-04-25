d6ec8fce0563174f4fb011b02ece0e26df08379c
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-04-25 21:58:55 +0700

    modified:   classes/helpers.py

--- classes/helpers.py
+++ classes/helpers.py
@@ -265,8 +265,8 @@ class Utils:
             input_ids += padding
             input_mask += padding
             segment_ids += padding
-            #print(len(input_ids))
-            #print(len(segment_ids))
+            print(len(input_ids))
+            print(len(segment_ids))
             assert len(input_ids) == max_sequence_length
             assert len(input_mask) == max_sequence_length
             assert len(segment_ids) == max_sequence_length
