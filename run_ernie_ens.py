ad0532553e6008354e5a38616cdd4109e7f6e589
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-05-12 10:12:29 +0200

    modified:   run_ernie_ens.py

--- run_ernie_ens.py
+++ run_ernie_ens.py
@@ -31,7 +31,7 @@ LOSS_FUNCTION = config["loss_function"]
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 pretrained_model='bert-base-uncased'
 TOKENIZER = AutoTokenizer.from_pretrained(pretrained_model)
-MAX_LENGTH = 39
+MAX_LENGTH = 42
 # define a rich console logger
 console=Console(record=True)
     
