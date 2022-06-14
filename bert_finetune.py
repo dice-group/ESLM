3989adcb31191784fe866b92b1f7e2e3ec826138
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-06-14 17:21:15 +0200

    modified:   bert_finetune.py

--- bert_finetune.py
+++ bert_finetune.py
@@ -103,9 +103,6 @@ def main(mode, best_epoch):
             for topk in config["topk"]:
                 dataset = ESBenchmark(ds_name, file_n, topk, is_weighted_adjacency_matrix)
                 test_data = dataset.get_testing_dataset()
-                fmeasure_scores = []
-                ndcg_scores = []
-                map_scores = []
                 for fold in range(5):
                     print("")
                     print(f"fold: {fold+1}, total entities: {len(test_data[fold][0])}", f"topk: top{topk}")
@@ -118,11 +115,8 @@ def main(mode, best_epoch):
                     model.bert_model.load_state_dict(checkpoint["bert_model"])
                     model.classifier.load_state_dict(checkpoint["classifier"])
                     model.to(DEVICE)
-                    fmeasure_score, ndcg_score, map_score = generated_entity_summaries(model, test_data[fold][0], dataset, topk, MAX_LENGTH)
-                    fmeasure_scores.append(fmeasure_score)
-                    ndcg_scores.append(ndcg_score)
-                    map_scores.append(map_score)
-                print(f"{dataset.ds_name}@top{topk}: F-Measure={np.average(fmeasure_scores)}, NDCG={np.average(ndcg_scores)}, MAP={np.average(map_scores)}")
+                    generated_entity_summaries(model, test_data[fold][0], dataset, topk, MAX_LENGTH)
+                evaluation(dataset, topk)
 def train(model, optimizer, train_data, valid_data, dataset, topk, fold, models_dir, max_length):
     """Training module"""
     if not os.path.exists(models_dir):
@@ -283,6 +277,7 @@ def generated_entity_summaries(model, test_data, dataset, topk, max_length):
             rank_list = output_rank.squeeze(0).numpy().tolist()
             writer(dataset.get_db_path, directory, eid, top_or_rank, topk, rank_list)
     return np.average(fmeasure_scores), np.average(ndcg_scores), np.average(map_scores)
+
 def writer(db_dir, directory, eid, top_or_rank, topk, rank_list):
     "Write triples to file"
     with open(os.path.join(db_dir, f"{eid}", f"{eid}_desc.nt"), encoding="utf8") as fin:
@@ -290,6 +285,107 @@ def writer(db_dir, directory, eid, top_or_rank, topk, rank_list):
             triples = [triple for _, triple in enumerate(fin)]
             for rank in rank_list:
                 fout.write(triples[rank])
+
+def get_rank_triples(db_path, num, top_n, triples_dict):
+  triples=[]
+  encoded_triples = []
+  filename = os.path.join(db_path, "{}".format(num), "{}_rank.nt".format(num))
+  if os.path.exists(os.path.join(db_path, "{}".format(num), "{}_rank_top{}.nt".format(num, top_n))):
+      filename = os.path.join(db_path, "{}".format(num), "{}_rank_top{}.nt".format(num, top_n))
+  with open(filename, encoding="utf8") as reader:   
+    for i, triple in enumerate(reader):
+        triple = triple.replace("\n", "").strip()
+        triples.append(triple)
+        
+        encoded_triple = triples_dict[triple]
+        encoded_triples.append(encoded_triple)
+  return triples, encoded_triples
+
+def get_topk_triples(db_path, num, top_n, triples_dict):
+  triples=[]
+  encoded_triples = []
+  
+  with open(os.path.join(db_path, "{}".format(num), "{}_top{}.nt".format(num, top_n)), encoding="utf8") as reader:   
+    for i, triple in enumerate(reader):
+        triple = triple.replace("\n", "").strip()
+        triples.append(triple)
+        
+        encoded_triple = triples_dict[triple]
+        encoded_triples.append(encoded_triple)
+  return triples, encoded_triples
+
+def evaluation(dataset, k):
+    ndcg_class = NDCG()
+    fmeasure = FMeasure()
+    m = MAP()
+    
+    if dataset == "dbpedia":
+        IN_SUMM = "outputs/dbpedia"
+        start = [0, 140]
+        end   = [100, 165]
+    elif dataset == "lmdb":
+        IN_SUMM = "outputs/lmdb"
+        start = [100, 165]
+        end   = [140, 175]
+            
+    all_ndcg_scores = []
+    all_fscore = []
+    all_map_scores = []
+    total_ndcg=0
+    total_fscore=0
+    total_map_score=0
+    for i in range(start[0], end[0]):
+        t = i+1
+        triples_dict = dataset.triples_dictionary(t)
+        gold_list_top = dataset.get_gold_summaries(t, triples_dict)
+        rank_triples, encoded_rank_triples = get_rank_triples(IN_SUMM, t, k, triples_dict)
+        topk_triples, encoded_topk_triples = get_topk_triples(IN_SUMM, t, k, triples_dict)
+        #print("############### Top-K Triples ################", t)
+        #print("######################")
+        #print(triples_dict)
+        #print("total of gold summaries", len(gold_list_top))
+        #print("topk", encoded_topk_triples)
+        #ndcg_score = getNDCG(rel)
+        ndcg_score = ndcg_class.get_score(gold_list_top, encoded_rank_triples)
+        f_score = fmeasure.get_score(encoded_topk_triples, gold_list_top)
+        map_score = m.get_avg_map(encoded_rank_triples, gold_list_top)
+        #print(ndcg_score)
+        #print("*************************")
+        total_ndcg += ndcg_score
+        all_ndcg_scores.append(ndcg_score)
+        
+        total_fscore += f_score
+        all_fscore.append(f_score)
+        
+        all_map_scores.append(map_score)
+        
+    for i in range(start[1], end[1]):
+        t = i+1
+        triples_dict = dataset.triples_dictionary(t)
+        gold_list_top = dataset.get_gold_summaries(t, triples_dict)
+        rank_triples, encoded_rank_triples = get_rank_triples(IN_SUMM, t, k, triples_dict)
+        topk_triples, encoded_topk_triples = get_topk_triples(IN_SUMM, t, k, triples_dict)
+        #print("############### Top-K Triples ################", t)
+        #print("######################")
+        #print(triples_dict)
+        #print("total of gold summaries", len(gold_list_top))
+        #print("topk", encoded_topk_triples)
+        #ndcg_score = getNDCG(rel)
+        ndcg_score = ndcg_class.get_score(gold_list_top, encoded_rank_triples)
+        f_score = fmeasure.get_score(encoded_topk_triples, gold_list_top)
+        map_score = m.get_avg_map(encoded_rank_triples, gold_list_top)
+        #print(ndcg_score)
+        #print("*************************")
+        total_ndcg += ndcg_score
+        all_ndcg_scores.append(ndcg_score)
+        
+        total_fscore += f_score
+        all_fscore.append(f_score)
+        
+        all_map_scores.append(map_score)
+        
+    print("{}@top{}: F-Measure={}, NDCG={}, MAP={}".format(dataset, k, np.average(all_fscore), np.average(all_ndcg_scores), np.average(all_map_scores)))
+
 def cls_cosine_distance(embeds):
     CLSs = embeds[:, 0, :]
     # normalize the CLS token embeddings
