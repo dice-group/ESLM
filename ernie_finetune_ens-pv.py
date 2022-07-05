dc884b25aeea4c70994887950a6f146f3f3b0fe3
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-07-05 02:18:20 +0200

    modified:   ernie_finetune_ens-pv.py

--- ernie_finetune_ens-pv.py
+++ ernie_finetune_ens-pv.py
@@ -90,20 +90,11 @@ def main(mode, best_epoch):
                 for fold in range(5):
                     print("")
                     print(f"fold: {fold+1}, total entities: {len(test_data[fold][0])}", f"topk: top{topk}")
-                    fmeasure_score, ndcg_score, map_score = generated_entity_summaries(test_data[fold][0], dataset, topk, fold, models, MAX_LENGTH)
-                    fmeasure_scores.append(fmeasure_score)
-                    ndcg_scores.append(ndcg_score)
-                    map_scores.append(map_score)
-                print(f"{dataset.ds_name}@top{topk}: F-Measure={np.average(fmeasure_scores)}, NDCG={np.average(ndcg_scores)}, MAP={np.average(map_scores)}")
+                    generated_entity_summaries(test_data[fold][0], dataset, topk, fold, models, MAX_LENGTH)
+                evaluation(dataset, topk)
 
 def generated_entity_summaries(test_data, dataset, topk, fold, models, max_length):
     """"Generated entity summaries"""
-    ndcg_eval = NDCG()
-    fmeasure_eval = FMeasure()
-    map_eval = MAP()
-    ndcg_scores = []
-    fmeasure_scores = []
-    map_scores = []
     with torch.no_grad():
         for eid in test_data:
             triples = dataset.get_triples(eid)
@@ -120,16 +111,6 @@ def generated_entity_summaries(test_data, dataset, topk, fold, models, max_lengt
             #(label_top_scores, label_top) = torch.topk(target_tensor, topk)
             _, output_top = torch.topk(output_tensor, topk)
             _, output_rank = torch.topk(output_tensor, len(test_data[eid]))
-            triples_dict = dataset.triples_dictionary(eid)
-            gold_list_top = dataset.get_gold_summaries(eid, triples_dict)
-            top_list_output_top = output_top.squeeze(0).numpy().tolist()
-            all_list_output_top = output_rank.squeeze(0).numpy().tolist()
-            ndcg_score = ndcg_eval.get_score(gold_list_top, all_list_output_top)
-            f_score = fmeasure_eval.get_score(top_list_output_top, gold_list_top)
-            map_score = map_eval.get_map(all_list_output_top, gold_list_top)
-            ndcg_scores.append(ndcg_score)
-            fmeasure_scores.append(f_score)
-            map_scores.append(map_score)
             directory = f"outputs/{dataset.get_ds_name}"
             if not os.path.exists(directory):
                 os.makedirs(directory)
@@ -142,7 +123,6 @@ def generated_entity_summaries(test_data, dataset, topk, fold, models, max_lengt
             top_or_rank = "rank_top"
             rank_list = output_rank.squeeze(0).numpy().tolist()
             writer(dataset.get_db_path, directory, eid, top_or_rank, topk, rank_list)
-    return np.average(fmeasure_scores), np.average(ndcg_scores), np.average(map_scores)
 
 def writer(db_dir, directory, eid, top_or_rank, topk, rank_list):
     "Write triples to file"
@@ -166,6 +146,146 @@ def ensemble_predictions(members, all_input_ids, all_input_mask, all_segment_ids
     yhats = torch.stack([model(all_input_ids, all_input_mask, all_segment_ids) for model in members])
     result = torch.sum(yhats, axis=0)
     return result
+
+def get_rank_triples(db_path, num, top_n, triples_dict):
+  triples=[]
+  encoded_triples = []
+  filename = os.path.join(db_path, "{}".format(num), "{}_rank.nt".format(num))
+  if os.path.exists(os.path.join(db_path, "{}".format(num), "{}_rank_top{}.nt".format(num, top_n))):
+      filename = os.path.join(db_path, "{}".format(num), "{}_rank_top{}.nt".format(num, top_n))
+  with open(filename, encoding="utf8") as reader:   
+    for i, triple in enumerate(reader):
+        triple = triple.replace("\n", "").strip()
+        triples.append(triple)     
+        encoded_triple = triples_dict[triple]
+        encoded_triples.append(encoded_triple)
+  return triples, encoded_triples
+
+def get_topk_triples(db_path, num, top_n, triples_dict):
+  triples=[]
+  encoded_triples = []
+  
+  with open(os.path.join(db_path, "{}".format(num), "{}_top{}.nt".format(num, top_n)), encoding="utf8") as reader:   
+    for i, triple in enumerate(reader):
+        triple = triple.replace("\n", "").strip()
+        triples.append(triple)
+        
+        encoded_triple = triples_dict[triple]
+        encoded_triples.append(encoded_triple)
+  return triples, encoded_triples
+
+
+def get_all_data(db_path, num, top_n, file_n):
+  import glob
+  triples_dict = {}
+  triple_tuples = []
+  ### Retrieve all triples of an entity based on eid
+  with open(os.path.join(db_path, "{}".format(num), "{}_desc.nt".format(num)), encoding="utf8") as reader:   
+    for i, triple in enumerate(reader):
+      if len(triple)==1:
+        continue  
+      triple_tuple = triple.replace("\n", "").strip()#parserline(triple)
+      triple_tuples.append(triple_tuple)
+      if triple_tuple not in triples_dict:
+        triples_dict[triple_tuple] = len(triples_dict)
+  gold_list = []
+  ds_name = db_path.split("/")[-1].split("_")[0]
+  
+  ### Get file_n/ n files of ground truth summaries for faces dataset
+  if ds_name=="faces":
+      gold_files = glob.glob(os.path.join(db_path, "{}".format(num), "{}_gold_top{}_*".format(num, top_n).format(num)))
+      #print(len(gold_files))
+      if len(gold_files) != file_n:
+          file_n = len(gold_files)
+  
+  ### Retrieve ground truth summaries of an entity based on eid and total of file_n  
+  for i in range(file_n):
+    with open(os.path.join(db_path, 
+            "{}".format(num), 
+            "{}_gold_top{}_{}.nt".format(num, top_n, i).format(num)),
+            encoding="utf8") as reader:
+      #print(path.join(db_path, "{}".format(num), "{}_gold_top{}_{}.nt".format(num, top_n, i).format(num)))
+      n_list = []
+      for i, triple in enumerate(reader):
+        if len(triple)==1:
+            continue
+        triple_tuple = triple.replace("\n", "").strip()#parserline(triple)
+        gold_id = triples_dict[triple_tuple]
+        n_list.append(gold_id)
+      gold_list.append(n_list)
+        
+  return gold_list, triples_dict, triple_tuples
+
+def evaluation(dataset, k):
+    ndcg_class = NDCG()
+    fmeasure = FMeasure()
+    m = MAP()
+    
+    if dataset.ds_name == "dbpedia":
+        IN_SUMM = os.path.join(os.getcwd(), 'outputs/dbpedia')
+        start = [0, 140]
+        end   = [100, 165]
+    elif dataset.ds_name == "lmdb":
+        IN_SUMM = os.path.join(os.getcwd(), 'outputs/lmdb')
+        start = [100, 165]
+        end   = [140, 175]
+            
+    all_ndcg_scores = []
+    all_fscore = []
+    all_map_scores = []
+    total_ndcg=0
+    total_fscore=0
+    total_map_score=0
+    for i in range(start[0], end[0]):
+        t = i+1
+        gold_list_top, triples_dict, triple_tuples = get_all_data(dataset.db_path, t, k, dataset.file_n)
+        rank_triples, encoded_rank_triples = get_rank_triples(IN_SUMM, t, k, triples_dict)
+        topk_triples, encoded_topk_triples = get_topk_triples(IN_SUMM, t, k, triples_dict)
+        #print("############### Top-K Triples ################", t)
+        #print("######################")
+        #print(triples_dict)
+        #print("total of gold summaries", len(gold_list_top))
+        #print("topk", encoded_topk_triples)
+        #ndcg_score = getNDCG(rel)
+        ndcg_score = ndcg_class.get_score(gold_list_top, encoded_rank_triples)
+        f_score = fmeasure.get_score(encoded_topk_triples, gold_list_top)
+        map_score = m.get_map(encoded_rank_triples, gold_list_top)
+        #print(ndcg_score)
+        #print("*************************")
+        total_ndcg += ndcg_score
+        all_ndcg_scores.append(ndcg_score)
+        
+        total_fscore += f_score
+        all_fscore.append(f_score)
+        
+        all_map_scores.append(map_score)
+        
+    for i in range(start[1], end[1]):
+        t = i+1
+        gold_list_top, triples_dict, triple_tuples = get_all_data(dataset.db_path, t, k, dataset.file_n)
+        rank_triples, encoded_rank_triples = get_rank_triples(IN_SUMM, t, k, triples_dict)
+        topk_triples, encoded_topk_triples = get_topk_triples(IN_SUMM, t, k, triples_dict)
+        #print("############### Top-K Triples ################", t)
+        #print("######################")
+        #print(triples_dict)
+        #print("total of gold summaries", len(gold_list_top))
+        #print("topk", encoded_topk_triples)
+        #ndcg_score = getNDCG(rel)
+        ndcg_score = ndcg_class.get_score(gold_list_top, encoded_rank_triples)
+        f_score = fmeasure.get_score(encoded_topk_triples, gold_list_top)
+        map_score = m.get_map(encoded_rank_triples, gold_list_top)
+        #print(ndcg_score)
+        #print("*************************")
+        total_ndcg += ndcg_score
+        all_ndcg_scores.append(ndcg_score)
+        
+        total_fscore += f_score
+        all_fscore.append(f_score)
+        
+        all_map_scores.append(map_score)
+        
+    print("{}@top{}: F-Measure={}, NDCG={}, MAP={}".format(dataset, k, np.average(all_fscore), np.average(all_ndcg_scores), np.average(all_map_scores)))
+
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='BERT-GATES')
