f8063c6b482a2ae49465ae01022e230cb5e894ed
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-07-07 18:38:53 +0200

    modified:   ernie_finetune.py

--- ernie_finetune.py
+++ ernie_finetune.py
@@ -65,7 +65,7 @@ def main(mode, best_epoch):
         if ds_name == "dbpedia":
             MAX_LENGTH = 39
         else:
-            MAX_LENGTH = 34
+            MAX_LENGTH = 32
         if mode == "train":
             for topk in config["topk"]:
                 dataset = ESBenchmark(ds_name, file_n, topk, is_weighted_adjacency_matrix)
@@ -82,7 +82,7 @@ def main(mode, best_epoch):
                         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
                         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
                         ]
-                    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=5e-5, eps=1e-8)
+                    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=2e-5, eps=1e-8)
                     models_path = os.path.join("models", f"ernie_checkpoint-{ds_name}-{topk}-{fold}")
                     models_dir = os.path.join(os.getcwd(), models_path)
                     train(model, optimizer, train_data[fold][0], valid_data[fold][0], dataset, topk, fold, models_dir, MAX_LENGTH)
