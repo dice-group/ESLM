42f05fb0fda2f96cb9f1cc00e17a60e6f7eb1853
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-07-07 15:40:56 +0200

    modified:   run_ernie_gates.py

--- run_ernie_gates.py
+++ run_ernie_gates.py
@@ -193,6 +193,7 @@ def main(mode, best_epoch):
                 for fold in range(5):
                     fold = fold
                     model = ErnieGAT()
+                    model.to(DEVICE)
                     #optimizer = torch.optim.Adam(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
                     param_optimizer = list(model.named_parameters())
                     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
@@ -200,7 +201,6 @@ def main(mode, best_epoch):
                         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
                         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
                         ]
-                    model.to(DEVICE)
                     optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=5e-5, eps=1e-8)
                     print("")
                     print(f"Fold: {fold+1}, total entities: {len(train_data[fold][0])}", f"topk: top{topk}")
